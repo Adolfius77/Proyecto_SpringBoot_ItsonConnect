929c10e91da5e159cb13d9712ccca9100baa25d3
Adolfo Ortega <jose.ortega252882@potros.itson.edu.mx>
2025-10-24 00:30:18 -0700

    agregue el controller chat

--- src/main/java/Controllers/ChatController.java
+++ src/main/java/Controllers/ChatController.java
@@ -1,12 +1,43 @@
 package Controllers;
 
+import DTOS.ChatMensajeDTO;
+import model.Mensaje;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import service.IMensajeService;
 
 @Controller
 public class ChatController {
     @Autowired
     private SimpMessagingTemplate messagingTemplate; //este se usa para enviar los mensajes a los clientes
+    @Autowired
+    private IMensajeService mensajeService;
 
+    @MessageMapping("/chat/{matchId}")
+    public void handleChatMessage(@DestinationVariable Long matchId, @Payload ChatMensajeDTO chatMensajeDTO) {        try{
+        chatMensajeDTO.setMatchId(matchId);
+        Mensaje mensajeGuardado = mensajeService.guardarMensaje(chatMensajeDTO);
+
+        ChatMensajeDTO respuestaDTO = new ChatMensajeDTO();
+        respuestaDTO.setContenido(mensajeGuardado.getContenido());
+        respuestaDTO.setEmisorId(mensajeGuardado.getEmisor().getId());
+        respuestaDTO.setEmisorNombre(mensajeGuardado.getEmisor().getNombre());
+        respuestaDTO.setMatchId(mensajeGuardado.getMatch().getId());
+
+        String destination = "/topic/match/" + matchId;
+        messagingTemplate.convertAndSend(destination, respuestaDTO);
+        System.out.println("mensaje reenviado a : " + destination);
+
+
+        }catch (Exception e){
+            System.err.println("Error procesando mensaje para match " + matchId + ": " + e.getMessage());
+            String errorDestination = "/user/" + chatMensajeDTO.getEmisorNombre() + "/queue/errors";
+            messagingTemplate.convertAndSend(errorDestination,"error al renviar: "+ e.getMessage());
+        }
+
+    }
 }
---
+++ src/main/java/service/IMensajeService.java
@@ -0,0 +1,13 @@
+package service;
+
+import DTOS.ChatMensajeDTO;
+import model.Mensaje;
+
+import java.util.List;
+
+public interface IMensajeService {
+    Mensaje guardarMensaje(ChatMensajeDTO dto)throws Exception;
+    Mensaje obtenerMensaje(Long id);
+    void eliminarMensaje(Long id) throws Exception;
+    List<Mensaje> listarMensajesPorMatch(Long idMatch, int limit);
+}

bc6b57c280368c98b37b11d98e9b8ed4648b5ec2
Adolfo Ortega <jose.ortega252882@potros.itson.edu.mx>
2025-10-23 23:04:40 -0700

    termine mi parte cree los repositorys ,corregi unas cosas en model y cree las dao, y una clase de service Signed-off-by:Adolfo Ortega <jose.ortega252882@potros.itson.edu.mx>

---
+++ src/main/java/service/IEstudianteService.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package service;
+
+import java.util.List;
+import model.Estudiante;
+
+/**
+ *
+ * @author USER
+ */
+public interface IEstudianteService {
+   Estudiante crearEstudiante(Estudiante estudiante)throws Exception;
+   Estudiante obtenerEstudiante(Long id);
+   Estudiante actualizarEstudiante(Estudiante estudiante) throws Exception;
+   void eliminarEstudiante(Long id) throws Exception;
+   List<Estudiante> listarEstudiantes(int limit);
+}

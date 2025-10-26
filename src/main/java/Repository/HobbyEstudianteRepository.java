07dcf156e827c464b581c9cc519dbdedaf675f17
bautistalunaluis45-jpg <bautistalunaluis45@gmail.com>
2025-10-25 19:18:39 -0700

    Origen

--- src/main/java/Repository/HobbyEstudianteRepository.java
+++ src/main/java/Repository/HobbyEstudianteRepository.java
@@ -6,7 +6,10 @@ import model.HobbyEstudiante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
-
+/**
+ * Hola
+ * @author luisb
+ */
 @Repository
 public interface HobbyEstudianteRepository extends JpaRepository<HobbyEstudiante, Long> {
     
